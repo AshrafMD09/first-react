7b7d94767d40b3b0f4a78f19383e9354c490c144
Shlayten <jaouane.mohamed.achraf@gmail.com>
2024-08-03 20:44:29 +0100

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+function Message(){
+    const name = "Ashraf"
+    return <h1>Hello {name}</h1>
+}
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+
+function ListGroup() {
+    const cities = ["New York", "Tokyo", "Berlin", "Paris"];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const creatItem = () => {cities.length > 0 ? console.log(cities.length) : null}
+  return (
+    <>
+      <h1>Popular Cities</h1>
+      {cities.length === 0 && <p>No Item found</p>}
+      <ul className="list-group">
+        {cities.map((item, index) => (
+          <li
+            className = {
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key = {item}
+            onClick={() => {setSelectedIndex(index)} }
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      {creatItem()}
+    </>
+  );
+}
+
+export default ListGroup;
+
+//8
